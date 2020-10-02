3d757b9cce3bc886fdf0994c0a2a1e2ac2f0767c
diodon <ekleins@gmail.com>
2020-10-02 16:16:51 +1000

    clean up

--- app.R
+++ app.R
@@ -1,8 +1,36 @@
 library(shiny)
+library(readr)
 library(ggplot2)
 library(dplyr)
 library(ggrepel)
 library(ggpubr)
+
+
+## read data
+df = read_csv("bkCurve_all.csv", col_types = cols())
+dfMD = read_csv("bkCurve_siteMetadata.csv", col_types = cols())
+siteList = unique(df$siteName)
+
+# Define UI
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Heat Accumulation Plot"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("site", "SITE:",
+                  siteList)
+    ),
+    
+    
+    # Show a plot 
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
 
 
 # Define server
