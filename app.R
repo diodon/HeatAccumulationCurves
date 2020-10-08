28364bdef202711a87cec5b6762af0ab59f9bf2d
diodon <ekleins@gmail.com>
2020-10-08 19:30:54 +1100

    polyfit is now monotonic

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 library(ggrepel)
 library(ggpubr)
+library(MonoPoly)
 options(dplyr.summarise.inform = FALSE)
 
 
@@ -51,33 +52,35 @@ server <- function(input, output) {
     
     ## fit polynomial for each location
     polyPredAll = data.frame()
-    for (location in topSLFLList){
-      model = lm(nDaysMean~poly(temp, 5), data=dfTop5, subset = SLFL==location)
-      tempList = sort(unique(dfTop5$temp[dfTop5$SLFL==location]))
-      polyPred = data.frame(SLFL = rep(location, length(tempList)), 
+    for (i in topSLFLList){
+      model = monpol(nDaysMean~temp, degree=5, data=dfTop5, subset = SLFL==i, monotone = "decreasing")
+      tempList = sort(unique(dfTop5$temp[dfTop5$SLFL==i]))
+      polyPred = data.frame(SLFL = rep(i, length(tempList)), 
                             temp=tempList, 
                             nDays = predict(model, data.frame(temp=tempList)))
+      names(polyPred)[3] = "nDays"
       polyPredAll = bind_rows(polyPredAll, polyPred)
     }
     
     #get point por labels
-    dfLabels = dfSite %>% group_by(year, SLFL) %>% summarise(temp = min(temp), nDays = nDays[which.min(temp)])
+    dfLabels = dfSite %>% group_by(year, SLFL) %>% 
+      summarise(temp = min(temp), nDays = nDays[which.min(temp)])
     
-    pp = ggplot(dfSite, aes(temp, nDays, group=factor(year)))
-    pp + geom_line(aes(colour=factor(year)), alpha=0.5, size=0.3) + 
-      geom_line(data=dfSite %>% filter(year==2017), aes(temp, nDays), colour="#feedde", size=1.5) + 
-      geom_line(data=dfSite %>% filter(year==2016), aes(temp, nDays), colour="#fdbe85", size=1.5) + 
-      geom_line(data=dfSite %>% filter(year==2002), aes(temp, nDays), colour="#fd8d3c", size=1.5) +
-      geom_line(data=dfSite %>% filter(year==1998), aes(temp, nDays), colour="#d94701", size=1.5) +
+    pp = ggplot(data=dfSite, aes(temp, nDays, group=factor(year)))
+    pp = pp + geom_line(aes(colour=factor(year)), alpha=0.5, size=0.3) + 
+      geom_line(data=dfSite %>% dplyr::filter(year==2017), aes(temp, nDays), colour="#feedde", size=1.5) + 
+      geom_line(data=dfSite %>% dplyr::filter(year==2016), aes(temp, nDays), colour="#fdbe85", size=1.5) + 
+      geom_line(data=dfSite %>% dplyr::filter(year==2002), aes(temp, nDays), colour="#fd8d3c", size=1.5) +
+      geom_line(data=dfSite %>% dplyr::filter(year==1998), aes(temp, nDays), colour="#d94701", size=1.5) +
       geom_line(data=polyPredAll, aes(temp, nDays, group=NA), colour="black", size=1.5) + ## plot the TOP5 average
       geom_text_repel(data=dfLabels, aes(label=year), size=3) + 
       labs(title=input$site, x="Temperature (°C)", y ="Number of days above temperature") + 
       facet_grid(SLFL~.) + 
       theme_pubclean() + 
       theme(legend.position="none")
-  },
-  height = 800)
-}
+    print((pp))
+    }, height = 800)
+  }
 
 
 
